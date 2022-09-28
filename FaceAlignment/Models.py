bcc0f311550367e30e064e0361e8b1acd939c2aa
sexozavr <daniildiveev@mail.ru>
2022-09-28 16:33:59 +0300

    [MODIFY] added FernCascadeand ShapeRegressor classes templates to Models

--- FaceAlignment/Models.py
+++ FaceAlignment/Models.py
@@ -10,6 +10,7 @@ class BoundingBox:
         self.height = 0
         self.centroid_x = 0
         self.centroid_y = 0
+        
 
 class Fern:
     def __init__(self,
@@ -43,5 +44,75 @@ class Fern:
     def write(self) -> None:
         raise NotImplementedError
     
+
+class FernCascade:
+    def __init__(self,
+                 ferns_:List[Fern],
+                 second_level_num_:int,) -> None:
+        raise NotImplementedError
+
+    def train(self, 
+              images:List[np.ndarray],
+              current_shapes:List[np.ndarray],
+              ground_truth_shapes:List[np.ndarray],
+              bounding_box:List[BoundingBox],
+              mean_shape:np.ndarray, 
+              second_level_num:int,
+              candidate_level_pixel_num:int,
+              fern_pixel_num:int, 
+              curr_level_num:int,
+              first_level_num:int,) -> List[np.ndarray]:
+        raise NotImplementedError
+
+    def predict(self,
+                image:np.ndarray,
+                bounding_box:BoundingBox,
+                mean_shape:np.ndarray,
+                shape:np.ndarray) -> np.ndarray:
+        raise NotImplementedError
     
-    
+    def read(self) -> None:
+        raise NotImplementedError
+
+    def write(self) -> None:
+        raise NotImplementedError
+
+
+class ShapeRegressor:
+    def __init__(self, 
+                 first_level_num_:int,
+                 landmark_num_:int,
+                 fern_cascades_: List[FernCascade],
+                 mean_shape_:np.ndarray, 
+                 training_shapes_:List[np.ndarray], 
+                 bounding_box_:List[BoundingBox],) -> None:
+        raise NotImplementedError
+
+    def train(self,
+              images:List[np.ndarray],
+              ground_truth_shapes:List[np.ndarray],
+              bounding_box:List[BoundingBox],
+              first_level_num:int,
+              second_level_num:int,
+              candidate_pixel_num:int, 
+              fern_pixel_num:int,
+              initial_num:int) -> None:
+        raise NotImplementedError
+
+    def predict(self,
+                image:np.ndarray, 
+                bounding_box:BoundingBox,
+                initial_num:int,) -> np.ndarray:
+        raise NotImplementedError
+
+    def read(self) -> None:
+        raise NotImplementedError
+
+    def write(self) -> None:
+        raise NotImplementedError
+
+    def load(self, path:str) -> None:
+        raise NotImplementedError
+
+    def save(self, path:str) -> None:
+        raise NotImplementedError
